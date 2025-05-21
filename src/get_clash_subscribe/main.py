b88e49005163e3c7a727ce8c532c393f2d2a3674
Frank Lin <honglin@duck.com>
2025-05-21 14:12:41 +0800

    update main

--- src/get_clash_subscribe/main.py
+++ src/get_clash_subscribe/main.py
@@ -6,17 +6,13 @@ import time
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-import feedparser
 import requests
+import yaml
 
 requests.packages.urllib3.disable_warnings()
 
 
 ok_code = [200, 201, 202, 203, 204, 205, 206]
-
-# 邮箱域名过滤列表
-blackhole_list = ["cnr.cn", "cyberpolice.cn", "gov.cn", "samr.gov.cn", "12321.cn"
-                  "miit.gov.cn", "chinatcc.gov.cn"]
 
 
 def write_log(content, level="INFO"):
@@ -27,7 +23,7 @@ def write_log(content, level="INFO"):
     with open(f'./log/{time.strftime("%Y-%m", time.localtime(time.time()))}-update.log', 'a', encoding="utf-8") as f:
         f.write(update_log)
 
-def get_subscribe_url():
+def get_subscribe_main():
     dirs = './subscribe'
     if not os.path.exists(dirs):
         os.makedirs(dirs)
@@ -35,53 +31,82 @@ def get_subscribe_url():
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    rss = feedparser.parse('https://www.cfmem.com/feeds/posts/default?alt=rss')
-    entries = rss.get("entries")
-    if not entries:
-        write_log("更新失败！无法拉取原网站内容", "ERROR")
+    clash_req = requests.get("https://cdn.honglin.ac.cn/statically/gh/ermaozi/get_subscribe/main/subscribe/clash.yml")
+    # 检查下载是否成功
+    if not clash_req.status_code in ok_code:
+        write_log("更新失败！无法拉取原订阅内容", "WARN")
         return
-    update_list = []
-    summary = entries[0].get("summary")
-    if not summary:
-        write_log("暂时没有可用的订阅更新", "WARN")
-        return
-    v2ray_list = re.findall(r">V2Ray/XRay -&gt; (.*?)</span>", summary)
-    # 获取普通订阅链接
-    if any(v2ray_list):
-        v2ray_url = v2ray_list[-1].replace('amp;', '')
-        v2ray_req = requests.request("GET", v2ray_url, verify=False)
-        v2ray_code = v2ray_req.status_code
-        if v2ray_code not in ok_code:
-            write_log(f"获取 v2ray 订阅失败：{v2ray_url} - {v2ray_code}", "WARN")
-        else:
-            update_list.append(f"v2ray: {v2ray_code}")
-            with open(dirs + '/v2ray.txt', 'w', encoding="utf-8") as f:
-                f.write(v2ray_req.text)
-    clash_list = re.findall(r">clash -&gt; (.*?)</span>", summary)
-    # 获取clash订阅链接
-    if any(clash_list):
-        clash_url = clash_list[-1].replace('amp;', '')
-        clash_req = requests.request("GET", clash_url, verify=False)
-        clash_code = clash_req.status_code
-        if clash_code not in ok_code:
-            write_log(f"获取 clash 订阅失败：{clash_url} - {clash_code}", "WARN")
-        else:
-            update_list.append(f"clash: {clash_code}")
-            with open(dirs + '/clash.yml', 'w', encoding="utf-8") as f:
-                clash_content = clash_req.content.decode("utf-8")
-                f.write(clash_content)
-    if update_list:
-        file_pat = re.compile(r"v2ray\.txt|clash\.yml")
-        if file_pat.search(os.popen("git status").read()):
-            write_log(f"更新成功：{update_list}", "INFO")
-        else:
-            write_log(f"订阅暂未更新", "WARN")
-    else:
-        write_log(f"未能获取新的更新内容", "WARN")
+    
+    try:
+        clash_content = clash_req.content.decode("utf-8")
+        clash_content_replaced = re.sub(r"https://raw.githubusercontent.com", "https://cdn.honglin.ac.cn/statically/gh", clash_content, flags=re.IGNORECASE)
 
+        node_free_proxies = get_node_free()
+        if node_free_proxies:
+            clash_yaml = yaml.safe_load(clash_content_replaced)
+            # 获取原订阅的节点列表
+            original_proxies = clash_yaml.get('proxies', [])
+            # 将原订阅的节点列表与node_free_proxies合并
+            original_proxies.extend(node_free_proxies)
+            # 更新原订阅的节点列表
+            clash_yaml['proxies'] = original_proxies
+
+            # 更新[♻️ 自动选择]列表
+            original_groups = clash_yaml.get('proxy-groups', [])
+            for group in original_groups:
+                if group.get('name') == '♻️ 自动选择':
+                    # 获取原订阅的节点列表
+                    original_auto_proxies = group.get('proxies', [])
+                    # 将原订阅的节点列表与node_free_proxies合并
+                    node_free_proxy_names = [item['name'] for item in node_free_proxies]
+                    original_auto_proxies.extend(node_free_proxy_names)
+                    # 更新原订阅的节点列表
+                    group['proxies'] = original_auto_proxies
+
+            # 将更新后的内容写回到YAML文件
+            clash_content_replaced = yaml.dump(clash_yaml, default_flow_style=False, allow_unicode=True)
+            
+        # 将更新后的内容写入文件
+        with open(dirs + '/clash.yml', 'w', encoding="utf-8") as f:
+            f.write(clash_content_replaced)
+            write_log(f"获取原订阅成功", "INFO")
+    except Exception as e:
+        write_log(f"订阅解析错误", "WARN")
+        return
+
+
+def get_node_free():
+    url = "https://cdn.honglin.ac.cn/statically/gh/Barabama/FreeNodes/main/nodes/nodefree.yaml"
+
+    # Send a GET request to the specified URL
+    response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the YAML content
+        try:
+            data = yaml.safe_load(response.content.decode('utf-8'))
+            proxies = data.get('proxies', [])
+            
+            for item in proxies:
+                # Check if the 'name' key exists and is not empty
+                if 'name' in item and item['name']:
+                    new_name = 'NF_' + item['name']
+                    item['name'] = new_name
+                else:
+                    print("No name found in the item")
+
+            return proxies
+        except yaml.YAMLError as e:
+            print(f"Error parsing YAML: {e}")
+            return
+
+    else:
+        print(f'Failed to retrieve the file: {response.status_code}')
+        return
 
 def main():
-    get_subscribe_url()
+    get_subscribe_main()
+    # get_node_free()
 
 
 # 主函数入口
