944576d76fc98226464e52e9a9b69c88c0e2f3ab
Frank <honglin@duck.com>
2025-08-06 13:37:29 +0800

    update cdn configs

--- public/userscript.js
+++ public/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         URI identity & styling
 // @namespace    scripts.frankindev.com
-// @version      0.1.26
+// @version      0.1.27
 // @description  try to take over the world with styles...
 // @author       Frank Lin
 // @match        *://*.baidu.com/*
@@ -17,8 +17,8 @@
 // @match        *://www.msn.cn/*
 // @match        *://www.reddit.com/*
 // @match        *://chatgpt.com/*
-// @icon         https://cdn.honglin.ac.cn/favicon.ico
-// @updateURL    https://cdn.honglin.ac.cn/statically/gh/flinhong/configs/main/public/userscript.js
+// @icon         https://pub.cdn.honglin.ac.cn/favicon.ico
+// @updateURL    https://pub.cdn.honglin.ac.cn/statically/gh/flinhong/configs/main/public/userscript.js
 // @grant        GM_addStyle
 // ==/UserScript==
 ;
@@ -28,7 +28,7 @@
   // add 'data-domain' attribute for css selector
   document.body.setAttribute("data-domain", window.location.hostname)
   const domain = window.location.hostname.replace(".com", "").replace(".co.uk", "").replace("www.", "").replace("cn.", "")
-  const styles = ["https://cdn.honglin.ac.cn/fonts/g/css?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Serif+SC:wght@200..900&family=Oswald:wght@200..700&family=Outfit:wght@100..900&display=swap", "https://cdn.honglin.ac.cn/statically/gh/flinhong/configs/main/public/styles/" + domain + ".css", ]
+  const styles = ["https://pub.cdn.honglin.ac.cn/fonts/g/css?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Serif+SC:wght@200..900&family=Oswald:wght@200..700&family=Outfit:wght@100..900&display=swap", "https://pub.cdn.honglin.ac.cn/statically/gh/flinhong/configs/main/public/styles/" + domain + ".css", ]
   styles.forEach((href) => {
     // Google fonts
     const dList = []
