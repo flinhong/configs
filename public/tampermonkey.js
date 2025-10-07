2bf5472b741f566a31735ea96d36c0ad99344a13
Frank Lin <honglin@duck.com>
2025-10-07 12:49:39 +0800

    update zhihu

--- public/tampermonkey.js
+++ public/tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         URI identity & styling
 // @namespace    scripts.frankindev.com
-// @version      0.1.33
+// @version      0.1.34
 // @description  try to take over the world with styles...
 // @author       Frank Lin
 // @match        *://*.baidu.com/*
