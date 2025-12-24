4c2f3f798dc2f6ac3373073663d41caf87aebb01
Frank Lin <honglin@duck.com>
2025-12-24 14:17:36 +0800

    update google styling

--- dist/tampermonkey.js
+++ dist/tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Styling Fonts
 // @namespace     https://github.com/flinhong/configs
-// @version       0.0.5
+// @version       0.0.6
 // @description   Styling fonts with CSS for better reading experience.
 // @author        Frank Lin
 // @icon          https://cdn.honglin.ac.cn/favicon.ico
