fa336ede928a34ca20561f3d1e609bd48d413017
Frank Lin <honglin@duck.com>
2026-01-19 03:49:21 +0000

    初始化仓库

---
+++ dist/tampermonkey.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name          Styling Fonts
+// @namespace     https://github.com/flinhong/configs
+// @version       0.0.12
+// @description   Styling fonts with CSS for better reading experience.
+// @author        Frank Lin
+// @icon          https://cdn.honglin.ac.cn/favicon.ico
+// @match         *://*.baidu.com/*
+// @match         *://google.com/*
+// @match         *://www.google.co.uk/*
+// @match         *://www.google.com/*
+// @run-at        document-end
+// @grant         GM_addStyle
+// @grant         GM_getResourceText
+// @downloadURL   https://cdn.honglin.ac.cn/statically/gh/flinhong/configs@main/dist/tampermonkey.js
+// @updateURL     https://cdn.honglin.ac.cn/statically/gh/flinhong/configs@main/dist/tampermonkey.meta.js
+// @resource      css_baidu https://cdn.honglin.ac.cn/statically/gh/flinhong/configs@main/dist/styles/baidu.css
+// @resource      css_google https://cdn.honglin.ac.cn/statically/gh/flinhong/configs@main/dist/styles/google.css
+// ==/UserScript==
---
+++ dist/userscripts.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          Styling Fonts
+// @namespace     https://github.com/flinhong/userscripts
+// @version       0.0.12
+// @description   Styling fonts with CSS for better reading experience.
+// @author        Frank Lin
+// @icon          https://cdn.honglin.ac.cn/favicon.ico
+// @match         *://*.baidu.com/*
+// @match         *://google.com/*
+// @match         *://www.google.co.uk/*
+// @match         *://www.google.com/*
+// @run-at        document-end
+// @downloadURL   https://cdn.honglin.ac.cn/statically/gh/flinhong/configs@main/dist/userscripts.js
+// @updateURL     https://cdn.honglin.ac.cn/statically/gh/flinhong/configs@main/dist/userscripts.meta.js
+// ==/UserScript==
